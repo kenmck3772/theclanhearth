7a0e1f6bf9aef98872e20411e3c9dd9ec00cbf32
kenmck3772 <48637368+kenmck3772@users.noreply.github.com>
2025-09-28 13:38:18 +0100

    Add files via upload

---
+++ js/data/tartanExamples.js
@@ -0,0 +1,6 @@
+export const tartanExamples = [
+  { name:"Placeholder", image:"./assets/images/tartans/tartan-placeholder.png" },
+  { name:"Modern", image:"./assets/images/tartans/tartan-modern.png" },
+  { name:"Ancient", image:"./assets/images/tartans/tartan-ancient.png" },
+  { name:"Hunting", image:"./assets/images/tartans/tartan-hunting.png" }
+];
