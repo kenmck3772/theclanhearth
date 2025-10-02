8f6da293d0bbca5492fcf746185e9190c65f676d
kenmck3772 <48637368+kenmck3772@users.noreply.github.com>
2025-10-02 02:21:24 +0100

    Delete js directory

--- js/data/clanTerritories.js
+++
@@ -1,1 +0,0 @@
-export const clanTerritories = [];
--- js/data/tartanExamples.js
+++
@@ -1,6 +0,0 @@
-export const tartanExamples = [
-  { name:"Placeholder", image:"./assets/images/tartans/tartan-placeholder.png" },
-  { name:"Modern", image:"./assets/images/tartans/tartan-modern.png" },
-  { name:"Ancient", image:"./assets/images/tartans/tartan-ancient.png" },
-  { name:"Hunting", image:"./assets/images/tartans/tartan-hunting.png" }
-];
