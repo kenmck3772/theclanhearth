2a2d7fcdf57c340052522ca5fe5c1b8412a78136
kenmck3772 <48637368+kenmck3772@users.noreply.github.com>
2025-10-10 04:28:12 +0100

    Update tartan-designer.js

--- js/tartan-designer.js
+++ js/tartan-designer.js
@@ -1,1 +1,10 @@
+// Tartan Designer functionality
+function initTartanDesigner() {
+    console.log('Initializing tartan designer...');
+    // Add your tartan designer-specific code here
+}
 
+// Export for use in main.js
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = { initTartanDesigner };
+}
