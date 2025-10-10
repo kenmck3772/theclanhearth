1055d9dc430383f6d28c1016a0ac64337aa3abfa
kenmck3772 <48637368+kenmck3772@users.noreply.github.com>
2025-10-10 04:44:28 +0100

    Update clans.js

--- js/clans.js
+++ js/clans.js
@@ -8,3 +8,13 @@ function initClans() {
 if (typeof module !== 'undefined' && module.exports) {
     module.exports = { initClans };
 }
+// Clans Module
+function initClans() {
+    console.log('Initializing clans module...');
+    // Add your clans-specific code here
+}
+
+// Make it globally available
+if (typeof window !== 'undefined') {
+    window.initClans = initClans;
+}
