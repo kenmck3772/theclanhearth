5da320267481a213c1ed939496a8100bbfd24300
kenmck3772 <48637368+kenmck3772@users.noreply.github.com>
2025-10-10 04:46:31 +0100

    Update recipes.js

--- js/recipes.js
+++ js/recipes.js
@@ -1,10 +1,10 @@
-// Recipes functionality
+// Recipes Module
 function initRecipes() {
-    console.log('Initializing recipes...');
+    console.log('Initializing recipes module...');
     // Add your recipes-specific code here
 }
 
-// Export for use in main.js
-if (typeof module !== 'undefined' && module.exports) {
-    module.exports = { initRecipes };
+// Make it globally available
+if (typeof window !== 'undefined') {
+    window.initRecipes = initRecipes;
 }
