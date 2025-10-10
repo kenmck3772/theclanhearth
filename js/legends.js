1b238e21675754b3986a0517123af825995d3058
kenmck3772 <48637368+kenmck3772@users.noreply.github.com>
2025-10-10 04:29:22 +0100

    Update legends.js

--- js/legends.js
+++ js/legends.js
@@ -1,1 +1,10 @@
+// Legends functionality
+function initLegends() {
+    console.log('Initializing legends...');
+    // Add your legends-specific code here
+}
 
+// Export for use in main.js
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = { initLegends };
+}
