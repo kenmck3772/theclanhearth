a69b9c0f13cb90d9d89701f4cb4cff6880d386e3
kenmck3772 <48637368+kenmck3772@users.noreply.github.com>
2025-10-10 04:25:07 +0100

    Update planner.js

--- js/planner.js
+++ js/planner.js
@@ -1,1 +1,11 @@
+// Planner functionality
+function initPlanner() {
+    console.log('Initializing planner...');
+    // Add your planner-specific code here
+}
+
+// Export for use in main.js
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = { initPlanner };
+}
 
