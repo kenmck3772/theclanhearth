aa667bfe5bc1544a35926afcc1384d43caa799c2
kenmck3772 <48637368+kenmck3772@users.noreply.github.com>
2025-10-10 04:26:50 +0100

    Update finders.js

--- js/finders.js
+++ js/finders.js
@@ -1,1 +1,10 @@
+// Finder functionality
+function initFinder() {
+    console.log('Initializing finder...');
+    // Add your finder-specific code here
+}
 
+// Export for use in main.js
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = { initFinder };
+}
